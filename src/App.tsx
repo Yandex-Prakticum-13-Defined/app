47b582977e7835d7300d04ee611852ff646185fa
Sergey Podryabinnikov <valentine94@inbox.ru>
2022-04-21 11:19:54 +0300

    Добавлен роутер

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './vendor/normalize.scss';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Мой апп.</h1>
-      </div>
+      <Router>
+        <div>
+          <h1>Тестовая страница</h1>
+        </div>
+      </Router>
     );
   }
 }
