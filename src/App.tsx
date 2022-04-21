7a0101fe5a17d5555e5b0cf8a2bdea16cd9d69e7
Sergey Podryabinnikov <valentine94@inbox.ru>
2022-04-21 10:31:21 +0300

    настроены webpack, eslint, stylelint, husky

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './vendor/normalize.scss';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Мой апп.</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    compress: true,
+    port: 8080,
+    open: true,
+    historyApiFallback: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(svg|woff(2)?)$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            configFile: path.resolve(__dirname, 'tsconfig.json'),
+          }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, {
+            loader: 'css-loader',
+            options: { importLoaders: 1 }
+          },
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './www/index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin()
+  ]
+};
