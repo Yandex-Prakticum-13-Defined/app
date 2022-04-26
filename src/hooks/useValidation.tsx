b17660c00b0199bd32dfa77f7c1793de339dfc3a
Tatiana <tttatttu@gmail.com>
2022-04-26 20:52:41 +0300

    feat [defined_19] Правка замечания eslint

--- src/hooks/useValidation.tsx
+++ src/hooks/useValidation.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import {useEffect, useState} from 'react';
 
 export const useValidation = (value: string, validations: any) => {
   const [isEmpty, setIsEmpty] = useState({
