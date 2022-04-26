7af00a44d1d444cb03143c72b27f7079c229d9b3
Tatiana <tttatttu@gmail.com>
2022-04-26 20:52:41 +0300

    feat [defined_19] Правка замечания eslint

--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -8,9 +8,15 @@ const Start = () => (
     <h1>Привет Арканоид</h1>
     <h2> Коротко о правилах игры</h2>
     <div className='start__button'>
-      <Button title='Войти' onClick={() => window.location.href = '/login'}/>
-      <Button title='Зарегистрироваться' onClick={() => window.location.href = '/register'}/>
-      <Button title='Начать игру' onClick={() => window.location.href = '/game'}/>
+      <Button title='Войти' onClick={() => {
+        window.location.href = '/login';
+      }}/>
+      <Button title='Зарегистрироваться' onClick={() => {
+        window.location.href = '/register';
+      }}/>
+      <Button title='Начать игру' onClick={() => {
+        window.location.href = '/game';
+      }}/>
     </div>
   </div>
 );
