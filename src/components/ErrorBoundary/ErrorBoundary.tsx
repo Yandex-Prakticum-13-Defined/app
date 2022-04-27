1772e226d772b7205c50fe2e1220bdb6904919f7
Alexander Korostelev <elsit-tm@yandex.ru>
2022-04-28 01:09:46 +0300

    defined-10 Реализован компонент ErrorBoundary

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,44 @@
+import React, { Component, ErrorInfo, ReactNode } from 'react';
+import image from '../../img/something-gone-wrong.png';
+import './ErrorBoundary.scss';
+
+interface IErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface IErrorBoundaryState {
+  hasError: boolean;
+}
+
+export class ErrorBoundary extends Component<IErrorBoundaryProps, IErrorBoundaryState> {
+  constructor(props: IErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // eslint-disable-next-line no-console
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div className='error-boundary'>
+          <img src={image} alt={'Ошибка!'} className='error-boundary__image' />
+          <div className='error-boundary__text-container'>
+            <h2 className='error-boundary__text'>Что-то пошло не так...</h2>
+            <p className='error-boundary__text'>Попробуйте перейти на главную или перезагрузить страницу</p>
+          </div>
+        </div>
+      );
+    }
+
+    return this.props.children;
+  }
+}
