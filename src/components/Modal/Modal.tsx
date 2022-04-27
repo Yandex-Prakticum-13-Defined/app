04be7b3c012bb765cbe6d642a6c273d93d39333a
Tatiana <tttatttu@gmail.com>
2022-04-27 13:31:34 +0300

    feat [defined_5] test8

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 import './Modal.scss';
 
 const Modal = () => (
-    <div className='modal'>
-      <h2 className='modal__title'>Game over</h2>
-      <Link className='modal__link' to='/'>На главную</Link>
-      <Link className='modal__link' to='/register'>Таблица лидеров</Link>
-      <Link className='modal__link' to='/game'>Повторить игру</Link>
-    </div>
+  <div className='modal'>
+    <h2 className='modal__title'>Game over</h2>
+    <Link className='modal__link' to='/'>На главную</Link>
+    <Link className='modal__link' to='/register'>Таблица лидеров</Link>
+    <Link className='modal__link' to='/game'>Повторить игру</Link>
+  </div>
 );
 
-export default Modal
+export default Modal;
