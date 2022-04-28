4d557fb4a128b66239b00a7603249ac72fcd4238
Sergey Podryabinnikov <valentine94@inbox.ru>
2022-04-28 22:48:01 +0300

    defined-29/refactor: замена функции sort на собственную

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.scss';
 import { ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary';
+import { sortOverload } from '../utils/sortOverload';
 
 const root = createRoot(document.querySelector('#root')!);
 root.render(<ErrorBoundary><App/></ErrorBoundary>);
+
+sortOverload();
