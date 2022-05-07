1106765a883331b6018f693845212e21ad2a5da8
Sergey Podryabinnikov <valentine94@inbox.ru>
2022-05-07 17:39:09 +0300

    defined-49/refactor: типизировали сортировку

--- src/utils/sortOverload.ts
+++ src/utils/sortOverload.ts
@@ -2,7 +2,7 @@ import quickSort, { defaultCompareFn, TCompareFn } from './quicksort';
 
 export function sortOverload() {
   // eslint-disable-next-line no-extend-native
-  Array.prototype.sort = function sortClone<T>(compareFn: TCompareFn = defaultCompareFn): T[] {
+  Array.prototype.sort = function sortClone<T>(compareFn: TCompareFn<string | number> = defaultCompareFn): T[] {
     return quickSort(this, compareFn);
   };
 }
