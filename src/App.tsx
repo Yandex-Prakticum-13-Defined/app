91a7152c2a40d1cc8df5bbf277e8c88a60f17588
Tatiana <tttatttu@gmail.com>
2022-05-17 14:46:26 +0300

    feat [defined_38] добавлен service worker

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.scss';
 import Auth from './hoc/Auth';
+import Offline from './pages/Offline/Offline';
 import Start from './pages/Start/Start';
 import Game from './pages/Game/Game';
 import Register from './pages/Register/Register';
@@ -14,6 +15,17 @@ import Forum from './pages/Forum/Forum';
 import ForumTopic from './pages/Forum/components/ForumTopic/ForumTopic';
 import ForumTopics from './pages/Forum/components/ForumTopics/ForumTopics';
 import { ERoutes } from './utils/constants/routes';
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', async () => {
+    try {
+      const registration = await navigator.serviceWorker.register('/sw.js');
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    } catch (error) {
+      console.log('ServiceWorker registration failed: ', error);
+    }
+  });
+}
 
 const App: FC = () => (
   <BrowserRouter>
@@ -30,6 +42,7 @@ const App: FC = () => (
       </Route>
       <Route path={ERoutes.ERROR_500} element={<Error500/>}/>
       <Route path={ERoutes.FALLBACK} element={<Error404/>}/>
+      <Route path={ERoutes.OFFLINE} element={<Offline/>}/>
     </Routes>
   </BrowserRouter>
 );
